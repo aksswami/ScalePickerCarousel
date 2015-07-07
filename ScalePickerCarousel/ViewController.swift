02d113d18e6411b4dc47f7ec366327e8df7fae14
Amit Kumar Swami <amitiitm2009@gmail.com>
2015-07-07 14:57:33 +0530

    Initial Commit

---
+++ ScalePickerCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScalePickerCarousel
+//
+//  Created by Amit kumar Swami on 07/07/15.
+//  Copyright (c) 2015 Aks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
